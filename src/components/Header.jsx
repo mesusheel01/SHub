5ef66f7f680716a786d4be01faaa875f1726cd40
Susheel <susheelrai96@gmail.com>
2024-06-30 21:08:37 +0530

    initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import logo from './../assets/Images/logo.png'
+const Header = () => {
+  return (
+    <div className='w-12 h-5 mx-4 my-4'>
+      <img src={logo} />
+    </div>
+  )
+}
+
+export default Header
